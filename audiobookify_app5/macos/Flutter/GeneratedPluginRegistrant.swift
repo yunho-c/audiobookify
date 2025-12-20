fc55f109ffe6006308462eaec9c7f2b4c29e5685
Yunho Cho <ycho358@gatech.edu>
2025-12-19 21:08:35 -0500

    chore: update `ios`/`macos` config files

--- audiobookify_app5/macos/Flutter/GeneratedPluginRegistrant.swift
+++ audiobookify_app5/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import file_picker
 import flutter_tts
 import objectbox_flutter_libs
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
